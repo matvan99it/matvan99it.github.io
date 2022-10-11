c5f82a8e15aebbf675ecb8ee948f671adf0ef79b
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-10-11 17:25:28 +0200

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,17 +1,1 @@
-
-
-const jsù = $.ajax({
-    url:"./project_list.json",
-    type:'GET',
-    //crossDomain: true,
-    dataType:'JSONP',
-    success: function(data){
-        console.log(data);
-        $('main').append( "Name: " + data );
-    },
-    fail: function(e){
-        console.log(e);
-    }
-});
-
-console.log(jsù);
+console.log(banana);
