92e39a1d0fedff921d377c584c911f00ec125b99
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2022-11-22 18:37:23 +0100

    aggiunta libreria animejs più modifiche

---
+++ JS/espansivo/app.js
@@ -0,0 +1,65 @@
+
+import anime from 'animejs/lib/anime.es.js';
+
+
+const colors = [
+    "rgb(229, 57,  53)",
+    "rgb(253, 216, 53)",
+    "rgb(244, 81,  30)",
+    "rgb(76,  175, 80)",
+    "rgb(33,  150, 243)",
+    "rgb(156, 39,  176)",
+];
+let count = -1;
+let columns = Math.floor(document.body.clientWidth /50), rows = Math.floor(document.body.clientHeight /50);
+const wrapper = document.getElementById("tiles");
+
+const handleOnClick = index => {
+    count = count + 1
+    anime({
+        targets: ".tile",
+        backgroundColor: colors[count % (colors.length - 1)]
+    })
+}
+
+const createTile = index => {
+    const  tile = document.createElement("div");
+    tile.classList.add("tile");
+
+    tile.onclick = e => handleOnClick(index);
+
+    return tile;
+}
+
+const createTiles = quantity => {
+    Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    })
+}
+
+createTiles(columns * rows);
+
+
+const craeteGrid = () => {
+    wrapper.innerHTML = "";
+    columns = Math.floor(document.body.clientWidth /50);
+    rows = Math.floor(document.body.clientHeight /50);
+    
+    
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
+    
+    
+    
+    
+    createTiles(columns * rows)
+
+
+
+
+
+}
+
+craeteGrid()
+
+window.onresize = () => craeteGrid()
